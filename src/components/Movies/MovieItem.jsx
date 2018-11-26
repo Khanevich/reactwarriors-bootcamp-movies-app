2ea257272b1f8ee023fd33725af31ac4263e7be2
Stas Khanevich <staskhanevich@gmail.com>
2018-11-26 23:06:40 +0200

    Add watch list

--- src/components/Movies/MovieItem.jsx
+++ src/components/Movies/MovieItem.jsx
@@ -6,7 +6,8 @@ export default class MovieItem extends React.Component {
   constructor() {
     super();
     this.state = {
-      heart: false
+      heart: false,
+      watch_list: false
     };
   }
 
@@ -35,9 +36,35 @@ export default class MovieItem extends React.Component {
     );
   };
 
+  onAddToWatch = () => {
+    const { user, session_id, item } = this.props;
+    this.setState({
+      watch_list: !this.state.watch_list
+    });
+    fetchApi(
+      `${API_URL}/account/${
+        user.id
+      }/watchlist?api_key=${API_KEY_3}&language=ru-RU&session_id=${session_id}`,
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json;charset=utf-8"
+        },
+        body: JSON.stringify({
+          media_type: "movie",
+          media_id: item.id,
+          watchlist: !this.state.watch_list
+        })
+      }
+    );
+  };
+
   render() {
     const { item } = this.props;
     const heartAccent = this.state.heart ? "far" : "fas";
+    const watch_listAccent = this.state.watch_list ? "far" : "fas";
+
     return (
       <div className="card" style={{ width: "100%" }}>
         <img
@@ -51,6 +78,9 @@ export default class MovieItem extends React.Component {
           <div className="card-text">Рейтинг: {item.vote_average}</div>
           <span className="heart" onClick={this.onAddFavorites}>
             <FontAwesomeIcon icon={[`${heartAccent}`, "heart"]} />
+          </span>
+          <span className="watch-list" onClick={this.onAddToWatch}>
+            <FontAwesomeIcon icon={[`${watch_listAccent}`, "bookmark"]} />
           </span>
         </div>
       </div>
