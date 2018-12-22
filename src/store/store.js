ccaa1807acf3e9e5412bf4c58b8a89a3645beb91
Stas Khanevich <staskhanevich@gmail.com>
2018-12-22 13:21:20 +0200

    Update react-scripts

---
+++ src/store/store.js
@@ -0,0 +1,25 @@
+// import { observable, action, computed } from "mobx";
+//
+// export default class Store {
+//   @observable
+//
+//    username: "",
+//
+//    @observable
+//     password: "",
+//
+//     @observable
+//     repeatPassword: "",
+//
+//     @observable
+//     errors: {
+//       username: false,
+//       password: false,
+//       repeatPassword: false,
+//       base: false
+//     }
+//
+//     @observable
+//     submitting: false
+//
+// }
