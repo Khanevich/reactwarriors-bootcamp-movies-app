331c69da9150e623fcdf8464782b7faebaa6638f
Stas Khanevich <staskhanevich@gmail.com>
2018-12-25 15:56:33 +0200

    Update store

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,7 @@ import CallApi, { API_URL, API_KEY_3, fetchApi } from "../api/api";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
+import MainStore from "../store/mainStore";
 
 library.add(fas, far);
 
@@ -17,148 +18,143 @@ const cookies = new Cookies();
 export const AppContext = React.createContext();
 
 export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      user: null,
-      session_id: null,
-      filters: {
-        sort_by: "popularity.desc",
-        primary_release_year: "2018",
-        with_genres: []
-      },
-      page: 1,
-      total_pages: "",
-      showLoginModal: false,
-      favoriteMovies: [],
-      watchList: []
-    };
-  }
-
-  getFavoriteMovies = () => {
-    const { session_id, user } = this.state;
-    CallApi.get(`/account/${user.id}/favorite/movies`, {
-      params: {
-        session_id: session_id,
-        language: "ru-RU"
-      }
-    })
-      .then(data => {
-        const moviesID = [];
-        data.results.map(movie => {
-          moviesID.push(movie.id);
-        });
-        this.setState({
-          favoriteMovies: [...moviesID]
-        });
-        return CallApi.get(`/account/${user.id}/watchlist/movies`, {
-          params: {
-            session_id: session_id,
-            language: "ru-RU"
-          }
-        });
-      })
-      .then(data => {
-        const moviesID = [];
-        data.results.map(movie => {
-          moviesID.push(movie.id);
-        });
-        this.setState({
-          watchList: [...moviesID]
-        });
-      });
-  };
-
-  logOut = (user, session_id) => {
-    fetchApi(`${API_URL}/authentication/session?api_key=${API_KEY_3}`, {
-      method: "DELETE",
-      mode: "cors",
-      headers: {
-        "Content-type": "application/json"
-      },
-      body: JSON.stringify({
-        session_id: this.state.session_id
-      })
-    })
-      .then(() =>
-        this.setState({
-          user: null,
-          session_id: null
-        })
-      )
-      .then(() => cookies.remove("session_id"));
-  };
-
-  toggleModal = () => {
-    console.log("toggle");
-    this.setState(prevState => ({
-      showLoginModal: !prevState.showLoginModal
-    }));
-  };
-
-  updateUser = user => {
-    this.setState({
-      user
-    });
-  };
-
-  updateSessionId = session_id => {
-    cookies.set("session_id", session_id, {
-      path: "/",
-      maxAge: 2592000
-    });
-
-    this.setState({
-      session_id
-    });
-  };
-
-  onChangeFilters = event => {
-    const value = event.target.value;
-    const name = event.target.name;
-    this.setState(prevState => ({
-      filters: {
-        ...prevState.filters,
-        [name]: value
-      }
-    }));
-  };
-
-  onChangePage = page => {
-    console.log(page);
-    this.setState({
-      page
-    });
-  };
-
-  onClear = event => {
-    this.setState({
-      filters: {
-        sort_by: "popularity.desc",
-        primary_release_year: "2018",
-        with_genres: []
-      },
-      page: 1
-    });
-  };
-
-  getTotalPages = total_pages => {
-    this.setState({
-      total_pages
-    });
-  };
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     user: null,
+  //     session_id: null,
+  //     filters: {
+  //       sort_by: "popularity.desc",
+  //       primary_release_year: "2018",
+  //       with_genres: []
+  //     },
+  //     page: 1,
+  //     total_pages: "",
+  //     showLoginModal: false,
+  //     favoriteMovies: [],
+  //     watchList: []
+  //   };
+  // }
+  //
+  // getFavoriteMovies = () => {
+  //   const { session_id, user } = this.state;
+  //   CallApi.get(`/account/${user.id}/favorite/movies`, {
+  //     params: {
+  //       session_id: session_id,
+  //       language: "ru-RU"
+  //     }
+  //   })
+  //     .then(data => {
+  //       const moviesID = [];
+  //       data.results.map(movie => {
+  //         moviesID.push(movie.id);
+  //       });
+  //       this.setState({
+  //         favoriteMovies: [...moviesID]
+  //       });
+  //       return CallApi.get(`/account/${user.id}/watchlist/movies`, {
+  //         params: {
+  //           session_id: session_id,
+  //           language: "ru-RU"
+  //         }
+  //       });
+  //     })
+  //     .then(data => {
+  //       const moviesID = [];
+  //       data.results.map(movie => {
+  //         moviesID.push(movie.id);
+  //       });
+  //       this.setState({
+  //         watchList: [...moviesID]
+  //       });
+  //     });
+  // };
+  //
+  // logOut = (user, session_id) => {
+  //   fetchApi(`${API_URL}/authentication/session?api_key=${API_KEY_3}`, {
+  //     method: "DELETE",
+  //     mode: "cors",
+  //     headers: {
+  //       "Content-type": "application/json"
+  //     },
+  //     body: JSON.stringify({
+  //       session_id: this.state.session_id
+  //     })
+  //   })
+  //     .then(() =>
+  //       this.setState({
+  //         user: null,
+  //         session_id: null
+  //       })
+  //     )
+  //     .then(() => cookies.remove("session_id"));
+  // };
+  //
+  // toggleModal = () => {
+  //   console.log("toggle");
+  //   this.setState(prevState => ({
+  //     showLoginModal: !prevState.showLoginModal
+  //   }));
+  // };
+  //
+  // updateUser = user => {
+  //   this.setState({
+  //     user
+  //   });
+  // };
+  //
+  // updateSessionId = session_id => {
+  //   cookies.set("session_id", session_id, {
+  //     path: "/",
+  //     maxAge: 2592000
+  //   });
+  //
+  //   this.setState({
+  //     session_id
+  //   });
+  // };
+  //
+  // onChangeFilters = event => {
+  //   const value = event.target.value;
+  //   const name = event.target.name;
+  //   this.setState(prevState => ({
+  //     filters: {
+  //       ...prevState.filters,
+  //       [name]: value
+  //     }
+  //   }));
+  // };
+  //
+  // onChangePage = page => {
+  //   console.log(page);
+  //   this.setState({
+  //     page
+  //   });
+  // };
+  //
+  // onClear = event => {
+  //   this.setState({
+  //     filters: {
+  //       sort_by: "popularity.desc",
+  //       primary_release_year: "2018",
+  //       with_genres: []
+  //     },
+  //     page: 1
+  //   });
+  // };
+  //
+  // getTotalPages = total_pages => {
+  //   this.setState({
+  //     total_pages
+  //   });
+  // };
 
   componentDidMount() {
-    const session_id = cookies.get("session_id");
-    if (session_id) {
-      fetchApi(
-        `${API_URL}/account?api_key=${API_KEY_3}&session_id=${session_id}`
-      ).then(user => {
-        this.updateUser(user);
-        this.updateSessionId(session_id);
-        this.getFavoriteMovies();
-      });
-    }
+    this.props.pageMount();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.props.pageUpdate();
   }
 
   render() {
